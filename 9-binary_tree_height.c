a7f560f7cea4b88b3d19f4caae90a5e4b0713bf2
ThatsVie <Viepaulalmt@gmail.com>
2023-12-14 16:22:24 -0800

    removing spaces

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -17,10 +17,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 	/* Recursively calculate the height of the left subtree */
 	left = binary_tree_height(tree->left);
-	
 	/* Recursively calculate the height of the right subtree */
 	right = binary_tree_height(tree->right);
-	
 	/* Return the maximum height of the left or right subtree */
 	/* plus 1 for the current node */
 	return (1 + (left > right ? left : right));
